02c6ef770542c33989d717120d5c0da9cd836fdb
Markus Krogemann <markus@krogemann.de>
2013-09-06 01:23:30 +0200

    use block to configure attributes

--- lib/try_until/probe.rb
+++ lib/try_until/probe.rb
@@ -1,15 +1,41 @@
 require 'timeout'
 
 module TryUntil
+  extend self
+
   class Probe
+
+    # attr_reader :tries, :interval, :timeout, :rescues, :condition
 
     def initialize(&block)
       # defaults
       @tries      = 3
       @interval   = 0
       @timeout    = :never
-      @rescue     = [StandardError, Timeout::Error]
-      @until      = Proc.new { true }
+      @rescues    = [ StandardError, Timeout::Error ]
+      @condition  = Proc.new { true }
+
+      instance_eval(&block)
+    end
+
+    def tries(tries)
+       @tries = tries
+    end
+
+    def interval(interval)
+      @interval = interval
+    end
+
+    def timeout(timeout)
+      @timeout = timeout
+    end
+
+    def rescues(rescues)
+      @rescues = rescues
+    end
+
+    def condition(condition)
+      @condition = condition
     end
   end
 end
--- spec/try-until/probe_spec.rb
+++ spec/try-until/probe_spec.rb
@@ -3,7 +3,22 @@ require 'spec_helper'
 module TryUntil
   describe Probe do
     describe '#initialize' do
+      it 'sets default values if no block is given' do
+        probe = Probe.new do
+        end
+        probe.instance_variable_get(:@tries).should == 3
+      end
 
+      it 'takes a block that contains its configuration' do
+        probe = Probe.new do
+          tries      7
+          interval   10
+          timeout    2.0
+          rescues    [ ArgumentError, Timeout::Error ]
+        end
+        probe.instance_variable_get(:@rescues).should == [ ArgumentError, Timeout::Error ]
+        probe.instance_variable_get(:@timeout).should == 2.0
+      end
     end
   end
 end
