324f8b5f55ac032bbbecfef45aa1a047f3bbe132
Markus Krogemann <markus@krogemann.de>
2013-09-07 23:19:24 +0200

    add documentation

--- lib/try_until/repeatedly.rb
+++ lib/try_until/repeatedly.rb
@@ -2,12 +2,23 @@ module TryUntil
   # External interface
   # Example:
   # include TryUntil
-  # Repeatedly.attempt do
+  # result = Repeatedly.attempt do
   #   probe       Probe.new(Object.new, :to_s)
   #   tries       5
   #   interval    10
+  #   rescues     [ ArgumentError, IOError ]
   #   condition   lambda { |response| JSON.parse(response.body)['id'] == 'some_id' }
   # end
+  #
+  # Not all of the above settings are required. These are the default values:
+  # probe      = nil
+  # tries      = 3
+  # interval   = 0
+  # rescues    = []
+  # condition  = lambda { false }
+  #
+  # If you forget to pass in a probe, you will receive a RuntimeError.
+  #
   class Repeatedly
     def self.attempt(&block)
       repeater = Repeater.new(&block)
