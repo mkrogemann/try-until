490b1a257f9c9d7aa942f6425d53135672c52445
Markus Krogemann <markus@krogemann.de>
2013-09-20 23:40:39 +0200

    waste less time by reducing poll interval

--- spec/try-until/repeatedly_spec.rb
+++ spec/try-until/repeatedly_spec.rb
@@ -20,14 +20,14 @@ module TryUntil
         time_then = Time.now
         probe = Probe.new(TestTarget.new, :inc)
         expected_result = lambda { |return_value| return_value == 4 }
-        Repeatedly.new(probe).attempts(5).interval(0.2).stop_when(expected_result).execute
-        expect(Time.now - time_then).to be > 0.7
+        Repeatedly.new(probe).attempts(5).interval(0.01).stop_when(expected_result).execute
+        expect(Time.now - time_then).to be > 0.04
       end
 
       it 'returns the expected result if the probe returns it within given number of attempts', :type => 'integration' do
         probe = Probe.new(TestTarget.new, :inc)
         expected_result = lambda { |return_value| return_value == 4 }
-        probe_returns = Repeatedly.new(probe).attempts(5).interval(0.2).stop_when(expected_result).execute
+        probe_returns = Repeatedly.new(probe).attempts(5).interval(0.01).stop_when(expected_result).execute
         probe_returns.should == 4
       end
 
